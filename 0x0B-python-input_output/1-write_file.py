686eca16722d49571be6a3f0305ca24e1689febc
Sibanengi <mpsibanengi@gmail.com>
2023-07-11 14:15:01 -0700

    student to json

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
+"""
+Contains the function "wrtie_file"
+"""
+
 
 def write_file(filename="", text=""):
-  """Writes a string to a text file (UTF8) and returns the number of characters written.
-
-  Args:
-    filename: The name of the file to write to.
-    text: The text to write to the file.
-
-  Returns:
-    The number of characters written to the file.
-  """
-
-  with open(filename, "w", encoding="utf-8") as f:
-    return f.write(text)
+    """returns the number of chars written to "filename" from "text" """
+    with open(filename, 'w', encoding='utf=8') as f:
+        return f.write(text)
